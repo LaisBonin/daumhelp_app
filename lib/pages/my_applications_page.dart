a6591ab2a5c555adbf807c947cef4ddf5019513e
Pedro Ganem <67203476+pedrocganem@users.noreply.github.com>
2022-04-12 16:37:04 -0300

    implements applied subjects list on my applications page

--- lib/pages/my_applications_page.dart
+++ lib/pages/my_applications_page.dart
@@ -1,10 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:daumhelp_app/widgets/return_button.dart';
 import 'package:daumhelp_app/widgets/subject_listtile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../widgets/theme_data.dart';
 
 class MyApplicationsPage extends StatelessWidget {
-  const MyApplicationsPage({Key? key}) : super(key: key);
+  const MyApplicationsPage({
+    Key? key,
+  }) : super(key: key);
+
+  Future<List> fetchAppliedSubjects() async {
+    final collection = FirebaseFirestore.instance.collection("users");
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser == null) {
+      return <String>[];
+    }
+    final user = await collection.doc(currentUser.uid).get();
+    final appliedSubjects = user["applies"];
+    return appliedSubjects;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,16 +58,43 @@ class MyApplicationsPage extends StatelessWidget {
               const SizedBox(
                 height: 16,
               ),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: 3,
-                itemBuilder: ((context, index) {
-                  return SubjectListTile(
-                      subjectName: "teste",
-                      subjectIcon: const Icon(Icons.add),
-                      subjectAction: () {});
-                }),
-              )
+              FutureBuilder<List>(
+                  future: fetchAppliedSubjects(),
+                  builder: (context, snapshot) {
+                    /// deu ruim
+                    if (snapshot.hasError) {
+                      const Text("Something went wrong");
+                    }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    }
+                    if (!snapshot.hasData) {
+                      print(snapshot.error);
+                      return const Text("error");
+                    }
+
+                    /// carregando
+                    if (snapshot.data!.isEmpty && !snapshot.hasError) {
+                      return const Text("Você ainda não tem candidaturas");
+                    }
+
+                    /// caminho feliz
+                    if (snapshot.hasData && !snapshot.hasError) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: ((context, index) {
+                          return SubjectListTile(
+                              subjectName: snapshot.data![index],
+                              subjectIcon: const Icon(Icons.add),
+                              subjectAction: () {});
+                        }),
+                      );
+                    } else {
+                      // sei la quye que deu
+                      return const CircularProgressIndicator();
+                    }
+                  })
             ],
           ),
         ),
