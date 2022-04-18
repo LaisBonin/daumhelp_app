1cb960d78019ac748c60b93f94f73c2f43a9aa65
LaisBonin <boninlaisc@gmail.com>
2022-04-17 22:40:04 -0300

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Da um help!',
       theme: HelpTheme.theme,
       home:  const LoginPageStl(),
     );
